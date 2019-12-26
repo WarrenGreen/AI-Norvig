55fda35063ee3164da2347354578dc677f72bb2c
WarrenGreen <s.green.warren@gmail.com>
2019-12-25 20:56:33 -0800

    breadth first search

--- ai/src/search/breadth_first_search.py
+++ ai/src/search/breadth_first_search.py
@@ -1,6 +1,3 @@
-from queue import PriorityQueue
-
-
 def search(initial_state, goal_state):
     """
 
@@ -14,15 +11,20 @@ def search(initial_state, goal_state):
     if initial_state is None or goal_state is None:
         raise ValueError("No valid path between initial state and goal state.")
 
-    frontier = PriorityQueue()
-    frontier.put((0, initial_state, []))
-    while not frontier.empty():
-        path_cost, node, path = frontier.get_nowait()
+    queue_1 = [(initial_state, [])]
+    queue_2 = []
+
+    while len(queue_1) > 0:
+        node, path = queue_1.pop(0)
         new_path = path + [node]
         if node == goal_state:
             return new_path
 
         for edge_cost, child_node in node.edges:
-            frontier.put((path_cost + edge_cost, child_node, new_path))
+            queue_2.append((child_node, new_path))
+
+        if len(queue_1) == 0:
+            queue_1 = queue_2
+            queue_2 = []
 
     raise ValueError("No valid path between initial state and goal state.")
---
+++ ai/src/search/depth_first_search.py
@@ -0,0 +1,2 @@
+def search(initial_state, goal_state):
+    pass
