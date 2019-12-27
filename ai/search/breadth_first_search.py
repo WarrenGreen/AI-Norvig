a63b134d464611be8b48dde2de2c5753f36f1b73
WarrenGreen <s.green.warren@gmail.com>
2019-12-26 19:37:30 -0800

    Simplifiy BFS and fix bfs class search method

--- ai/search/breadth_first_search.py
+++ ai/search/breadth_first_search.py
@@ -15,8 +15,14 @@ class BFS:
             raise InputException("Initial state and goal state cannot be None.")
 
     def search(self):
-        while len(self.queue_1) > 0:
+        while not self.is_finished:
             self.step()
+
+        if self.finished_path is not None:
+            return self.finished_path
+        else:
+            raise NoValidPathException(
+                "No valid path between initial state and goal state.")
 
     @property
     def is_finished(self):
@@ -31,7 +37,8 @@ class BFS:
             self.finished_path = new_path
 
         for edge_cost, child_node in node.edges:
-            self.queue_2.append((child_node, new_path))
+            if child_node not in self.explored:
+                self.queue_2.append((child_node, new_path))
 
         if len(self.queue_1) == 0:
             self.queue_1 = self.queue_2
@@ -48,26 +55,4 @@ def search(initial_state, goal_state):
     Returns:
         List[GraphNode] - optimal path from initial_state to goal_state
     """
-    if initial_state is None or goal_state is None:
-        raise InputException("Initial state and goal state cannot be None.")
-
-    explored = set()
-    queue_1 = [(initial_state, [])]
-    queue_2 = []
-
-    while len(queue_1) > 0:
-        node, path = queue_1.pop(0)
-        explored.add(node)
-        new_path = path + [node]
-        if node == goal_state:
-            return new_path
-
-        for edge_cost, child_node in node.edges:
-            if child_node not in explored:
-                queue_2.append((child_node, new_path))
-
-        if len(queue_1) == 0:
-            queue_1 = queue_2
-            queue_2 = []
-
-    raise NoValidPathException("No valid path between initial state and goal state.")
+    return BFS(initial_state, goal_state).search()
