d96521c1e4b6a4a3c1b430a40651fd12a549c0e9
WarrenGreen <s.green.warren@gmail.com>
2019-12-27 20:06:36 -0800

    Refactor maze example

--- examples/maze_search.py
+++ examples/maze_search.py
@@ -2,7 +2,7 @@ from ai.search.breadth_first_search import search
 from ai.search.exception import InputException
 from ai.search.node import GraphNode
 
-maze = [
+maze_1 = [
     ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
     ["O", "O", "O", "O", "X", "O", "O", "O", "O", "O", "X", "X", "X", "O", "O"],
     ["O", "O", "O", "X", "X", "O", "O", "O", "O", "O", "X", "O", "X", "O", "O"],
@@ -22,7 +22,7 @@ maze = [
 ]
 
 
-def main():
+def convert_maze_to_nodes(maze):
     nodes = {}
     for i in range(len(maze)):
         for j in range(len(maze[0])):
@@ -31,18 +31,23 @@ def main():
             node = GraphNode(f"({i},{j})")
             nodes[f"({i},{j})"] = node
             for ii, jj in ((0, -1), (0, 1), (-1, 0), (1, 0)):
-                if i+ii < 0 or i + ii >= len(maze) or j + jj < 0 or j + jj >= len(maze[0]):
+                if i + ii < 0 or i + ii >= len(maze) or j + jj < 0 or j + jj >= len(
+                        maze[0]):
                     continue
 
-                key = f"({i+ii},{j+jj})"
+                key = f"({i + ii},{j + jj})"
                 if key in nodes:
                     try:
                         node.add_edge(1, nodes[key])
                     except InputException:
                         pass
+    return nodes
 
-    start = nodes[f"({len(maze)-1},{0})"]
-    end = nodes[f"({0},{len(maze[0])-1})"]
+
+def main():
+    nodes = convert_maze_to_nodes(maze_1)
+    start = nodes[f"({len(maze_1)-1},{0})"]
+    end = nodes[f"({0},{len(maze_1[0])-1})"]
 
     print(search(start, end))
 
