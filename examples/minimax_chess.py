75493b000832b50b33a521f50610b6a8426a9ca6
WarrenGreen <s.green.warren@gmail.com>
2020-01-15 19:22:25 -0800

    Update minimax chess example with depth and alpha_beta config

--- examples/minimax_chess.py
+++ examples/minimax_chess.py
@@ -1,14 +1,36 @@
-from ai.search.adversarial.minimax import search
+import argparse
+
+from ai.search.adversarial.minimax import search as search_minimax
+from ai.search.adversarial.alpha_beta import search as search_alpha_beta
 from ai.search.problem.chess import Chess
 
 
-def main():
+def main(config):
     problem = Chess()
     state = problem.create_start()
     while not problem.is_terminal(state):
-        new_state, new_value = search(problem, state)
+        if config.alpha_beta:
+            new_state, new_value = search_alpha_beta(
+                problem, state, max_depth=config.depth
+            )
+        else:
+            new_state, new_value = search_minimax(
+                problem, state, maxthon_depth=config.depth
+            )
         state = new_state
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Have two chess agents play each other."
+    )
+    parser.add_argument(
+        "--alpha_beta", action="store_true", help="Use Alpha-Beta",
+    )
+    parser.add_argument(
+        "--depth",
+        type=int,
+        default=None,
+        help="Maximum number of plys for each agent to search.",
+    )
+    main(parser.parse_args())
